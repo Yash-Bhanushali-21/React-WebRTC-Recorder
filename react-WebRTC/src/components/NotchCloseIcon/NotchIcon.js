c47b91d2be43cac5b460aa9424ff9e8dc1a8ad3a
Yash Bhanushali <yash.bhanushali@ayraa.io>
2023-01-01 16:16:18 +0530

    refactored code for Notch and Tooltip components

--- react-WebRTC/src/components/NotchCloseIcon/NotchIcon.js
+++ react-WebRTC/src/components/NotchCloseIcon/NotchIcon.js
@@ -1,13 +1,18 @@
 import styles from "./styles/notchclose.module.css";
 import {IoCloseSharp } from "react-icons/io5";
+import React from "react";
 
-
-const NotchCloseIcon = ({handleClose}) => {
-    return (
-        <div className={styles.notch} role="button" onClick={handleClose}>
-            <IoCloseSharp className={styles.crossIcon} />
-        </div>
-    )
+class NotchCloseIcon extends React.Component {
+    constructor(props) {
+        super(props);
+        }
+    render() {
+        return (
+            <div className={styles.notch} role="button" onClick={this.props.handleClose}>
+                <IoCloseSharp className={styles.crossIcon} />
+            </div>
+        )
+    }
 }
 
 export default NotchCloseIcon;
--- react-WebRTC/src/components/TooltipOverlay/TooltipOverlay.js
+++ react-WebRTC/src/components/TooltipOverlay/TooltipOverlay.js
@@ -1,22 +1,33 @@
+import React from 'react';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Tooltip from 'react-bootstrap/Tooltip';
 import styles from "./styles/tooltipoverlay.module.css";
 
+export class TooltipOverlay extends React.Component {
 
-export const TooltipOverlay = ({tooltipLabel , content , show , placement="bottom" }) => {
-   
-  if(show) {
-    return(
-      <OverlayTrigger       
-        delay={{ show: 500, hide: 200 }}
-        offset={[0,10]}
-        rootClose
-        placement={placement} 
-        trigger={["hover","focus"]} 
-        overlay={<Tooltip style={{position : "absolute"}}>{tooltipLabel}</Tooltip>}>
-           {content}
-      </OverlayTrigger>
-      )
+  constructor(props) {
+    super(props);
+    
   }
-  return content;   
+
+  render() {
+
+    const {tooltipLabel , content , show , placement="bottom" } = this.props;
+    if(show) {
+      return(
+        <OverlayTrigger       
+          delay={{ show: 500, hide: 200 }}
+          offset={[0,10]}
+          rootClose
+          placement={placement} 
+          trigger={["hover","focus"]} 
+          overlay={<Tooltip style={{position : "absolute"}}>{tooltipLabel}</Tooltip>}>
+             {content}
+        </OverlayTrigger>
+        )
+    }
+    return content;   
+
+  }
 }
+
