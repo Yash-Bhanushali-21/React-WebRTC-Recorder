684766f0bc281daed3ca33a09af9a5d095c3ffce
Yash Bhanushali <yashbhadra1@gmail.com>
2023-11-09 20:49:08 +0530

    minor change

--- react-WebRTC/src/App.js
+++ react-WebRTC/src/App.js
@@ -1,55 +1,42 @@
-
-import React ,{ useState} from "react";
-
+import React, { useState } from "react";
 
 //custom component import.
-import {AudioAnalyzerModal} from "./components/AudioAnalyzer";
-import {ScreenRecorderModal} from "./components/ScreenRecorder";
+import { AudioAnalyzerModal } from "./components/AudioAnalyzer";
+import { ScreenRecorderModal } from "./components/ScreenRecorder";
 //icon imports
-import {HiOutlineVideoCamera } from "react-icons/hi2";
-import {  AiOutlineAudio } from "react-icons/ai";
+import { HiOutlineVideoCamera } from "react-icons/hi2";
+import { AiOutlineAudio } from "react-icons/ai";
 
 //css imports.
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
-
-
 function App() {
-
-  const [selectedScreen , setSelectedScreen] = useState(-1);
+  const [selectedScreen, setSelectedScreen] = useState(-1);
 
   const onClick = (screenIndex) => {
     setSelectedScreen(screenIndex);
-  }
- 
+  };
+
   function resetState() {
-        setSelectedScreen(-2); 
+    setSelectedScreen(-2);
   }
 
-
-  
-   return (<div className={"outerContainer"}>
-            <div className={"recorderContainer"} onClick={() => onClick(0)}>
-              <div className={"iconContainer"}><HiOutlineVideoCamera /></div>
-              <div className="lineSeperator"></div>
-              <div className={"titleContainer"}>Record</div>
-              <div className={"textContainer"}>let's you record a video for generating thumbnails </div>
-            </div>
-            
-            <div className={"thumbnailContainer"} onClick={() => onClick(1)}>
-              <div className={"iconContainer"}><AiOutlineAudio /></div>
-              <div className="lineSeperator"></div>
-              <div className={"titleContainer"}>Audio</div>
-              <div className={"textContainer"}>let's you record an audio with visualizations</div>
-              {/**               <PickerThumbnailGenerator show={true} close = {resetState} />
- */}
-            </div>
-           {selectedScreen === 0 &&  <ScreenRecorderModal show={selectedScreen === 0} close={resetState} />}
-           {selectedScreen === 1 &&  <AudioAnalyzerModal show = {selectedScreen === 1} close={resetState} />}
-
-   </div> )
-
+  return (
+    <div className={"outerContainer"}>
+      <div className={"recorderContainer"} onClick={() => onClick(0)}>
+        <div className={"iconContainer"}>
+          <HiOutlineVideoCamera />
+        </div>
+        <div className="lineSeperator"></div>
+        <div className={"titleContainer"}>Record</div>
+        <div className={"textContainer"}>
+          let's you record a video for generating thumbnails{" "}
+        </div>
+      </div>
+      <ScreenRecorderModal show={selectedScreen === 0} close={resetState} />
+    </div>
+  );
 }
 
 export default App;
--- react-WebRTC/src/components/ScreenRecorder/ScreenRecorderModal.js
+++ react-WebRTC/src/components/ScreenRecorder/ScreenRecorderModal.js
@@ -4,30 +4,61 @@ import ScreenRecorder from "./ScreenRecorder";
 import PreviewRecording from "./PreviewRecording";
 import ThumbnailSelection from "./ThumbnailSelection";
 
-const renderCurrentScreen = ({view, setView , close, recordedMedia, setRecordedMedia}) => {
-    switch(view){
-        case "record":
-            return <ScreenRecorder close={close} setRecordedMedia={setRecordedMedia} setView={setView} />
-        case "preview":
-            return <PreviewRecording close={close} recordedMedia={recordedMedia}  setView={setView} />
-        case "thumbnail":
-            return <ThumbnailSelection close={close} setRecordedMedia={setRecordedMedia} recordedMedia={recordedMedia} setView={setView} />
-        default:
-            return <div>{"Nothing to show."}</div>
-    }
-}
+const renderCurrentScreen = ({
+  view,
+  setView,
+  close,
+  recordedMedia,
+  setRecordedMedia,
+}) => {
+  switch (view) {
+    case "record":
+      return (
+        <ScreenRecorder
+          close={close}
+          setRecordedMedia={setRecordedMedia}
+          setView={setView}
+        />
+      );
+    case "preview":
+      return (
+        <PreviewRecording
+          close={close}
+          recordedMedia={recordedMedia}
+          setView={setView}
+        />
+      );
+    case "thumbnail":
+      return (
+        <ThumbnailSelection
+          close={close}
+          setRecordedMedia={setRecordedMedia}
+          recordedMedia={recordedMedia}
+          setView={setView}
+        />
+      );
+    default:
+      return <div>{"Nothing to show."}</div>;
+  }
+};
 
-const ScreenRecorderModal = ({show , close}) => {
+const ScreenRecorderModal = ({ show, close }) => {
+  const [view, setView] = useState("record");
+  const [recordedMedia, setRecordedMedia] = useState({
+    url: null,
+    thumbnail: null,
+  });
 
-    const [view, setView] = useState("record");
-    const [recordedMedia , setRecordedMedia] = useState({
-        url : null,
-        thumbnail : null
-    });
-
-    return (<Modal  show={show} dialogClassName={'modal-dialog'} animation={false} onHide={close} centered>
-               {renderCurrentScreen({view , setView ,recordedMedia, setRecordedMedia , close})} 
-          </Modal>)
-
-}
+  return (
+    <Modal show={show} dialogClassName={"modal-dialog"} onHide={close} centered>
+      {renderCurrentScreen({
+        view,
+        setView,
+        recordedMedia,
+        setRecordedMedia,
+        close,
+      })}
+    </Modal>
+  );
+};
 export default ScreenRecorderModal;
